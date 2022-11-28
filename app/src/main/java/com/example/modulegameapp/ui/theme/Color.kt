949b4cb0c0b5ee51457a8aab11f2c1ed0ed0aac8
Mie Agervig <agervig@live.dk>
2022-11-28 21:19:30 +0100

    Setup theme with temp colors

--- app/src/main/java/com/example/modulegameapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/modulegameapp/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.modulegameapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightPrimary = Color(0xFF1D9160)
+val LightBackground = Color(0xFFF2F5F7)
+val LightSurface = Color(0xFFFEFFFF)
+val LightSurface2 = Color(0xFFE4E5E6)
+val LightOnBackground = Color(0xCC000000)
+val LightOnSurface = Color(0xA6000000)
+val LightOnSurface2 = Color(0x80000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPrimary = Color(0xFF72CAA0)
+val DarkBackground = Color(0xFF49555C)
+val DarkSurface = Color(0xFF3B3B3B)
+val DarkSurface2 = Color(0xFF7A7B7C)
+val DarkOnBackground = Color(0xCCFFFFFF)
+val DarkOnSurface = Color(0xA6FFFFFF)
+val DarkOnSurface2 = Color(0x80FFFFFF)
